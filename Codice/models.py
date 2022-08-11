569866468469f4dbb24f9256aea20ca648891c73
Stefano Masiero <58975318+St3f4n0Masi3r0@users.noreply.github.com>
2022-08-11 11:31:39 +0200

    Update models.py

--- Codice/models.py
+++ Codice/models.py
@@ -1,179 +1,133 @@
-import sqlalchemy
-from sqlalchemy import *
-
-# USERS
-
-
-class User():
-    __tablename__ = 'users'                   # obbligatorio
-
-    # almeno un attributo deve fare parte della primary key
-    username = Column(String, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
-    birthdate = Column(Date)
-    password = Column(String)
-    gender = Column(String)
-    phone = Column(Integer)
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-class NormalListener():
-    __tablename__ = 'normallisteners'                   # obbligatorio
-
-    username = Column(String, primary_key=True, foreign_key=User.username)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-class PremiumListener():
-    __tablename__ = 'premiumlisteners'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-class Artist():
-    __tablename__ = 'artists'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-# SONGS
-
-class Song():
-    __tablename__ = 'songs'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-class NormalSong():
-    __tablename__ = 'normalsongs'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-class PremiumSong():
-    __tablename__ = 'premiumsongs'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-# STATISTICS
-
-
-class Statistic():
-    __tablename__ = 'statistics'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-# GENERES
-
-class Genre():
-    __tablename__ = 'genres'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-# verso artist
-
-
-class Relate():
-    __tablename__ = 'relate'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-# verso song
-
-
-class Belong():
-    __tablename__ = 'belong'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-# CREATES
-
-class Creates():
-    __tablename__ = 'creates'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-# ALBUMS
-
-class Album():
-    __tablename__ = 'albums'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-# PLAYLIST
-
-class Playlist():
-    __tablename__ = 'playlists'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
-
-
-#contain in playlist
-
-class Contains():
-    __tablename__ = 'contains'                   # obbligatorio
-
-    email = Column(String)
-
-    # questo metodo è opzionale, serve solo per pretty printing
-    def __repr__(self):
-        return "<User(username='%s', name='%s', surname='%s',birthdate='%s' password='%s', gender='%s',phone='%d',email='%s')>" % (self.username, self.name, self.surname, self.birthdate, self.password, self.gender, self.phone, self.email)
+CREATE TABLE Users
+(
+  Username VARCHAR(40) NOT NULL,
+  Name VARCHAR(20) NOT NULL,
+  Surname VARCHAR(20) NOT NULL,
+  Birthdate DATE NOT NULL,
+  Password VARCHAR(20) NOT NULL CHECK(LENGTH(Password)>=8),
+  Gender CHAR(1) NOT NULL CHECK(Gender = 'F' OR Gender = 'M'),
+  Phone INT NOT NULL,
+  Email VARCHAR(40) NOT NULL UNIQUE,
+  PRIMARY KEY (Username)
+);
+
+CREATE TABLE NormalListeners
+(
+  Username VARCHAR(40) NOT NULL,
+  PRIMARY KEY (Username),
+  FOREIGN KEY (Username) REFERENCES Users(Username)
+);
+
+CREATE TABLE PremiumListeners
+(
+  Username VARCHAR(40) NOT NULL,
+  PRIMARY KEY (Username),
+  FOREIGN KEY (Username) REFERENCES Users(Username)
+);
+
+CREATE TABLE Artists
+(
+  Username VARCHAR(40) NOT NULL,
+  PRIMARY KEY (Username),
+  FOREIGN KEY (Username) REFERENCES Users(Username)
+);
+
+CREATE TABLE Albums(
+  IDAlbum INT NOT NULL,
+  Name VARCHAR(40) NOT NULL,
+  Cover VARCHAR(40),
+  Artist VARCHAR(40) NOT NULL,
+
+  PRIMARY KEY (IDAlbum),
+  FOREIGN KEY (Artist) REFERENCES Artists(Username)
+);
+
+CREATE TABLE Songs
+(
+  Name VARCHAR(40) NOT NULL,
+  IDSong INT NOT NULL,
+  Album INT,
+  Cover VARCHAR(40),
+  ReleaseDate DATE NOT NULL,
+  Content VARCHAR(80),
+  PRIMARY KEY (IDSong),
+  FOREIGN KEY (Album) REFERENCES Albums(IDAlbum)
+);
+
+
+
+CREATE TABLE NormalSongs
+(
+  Song INT NOT NULL,
+  PRIMARY KEY (Song),
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong)
+);
+
+CREATE TABLE PremiumSongs
+(
+  ExpiryDate INT NOT NULL,
+  Song INT NOT NULL,
+  PRIMARY KEY (Song),
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong)
+);
+
+CREATE TABLE Statistics
+(
+  Song INT NOT NULL,
+  Upvote INT NOT NULL,
+  Downvote INT NOT NULL,
+  Views INT NOT NULL,
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong)
+);
+
+CREATE TABLE Playlists
+(
+  Name VARCHAR(20) NOT NULL,
+  IDList INT NOT NULL,
+  CreationDate DATE NOT NULL,
+  Author VARCHAR(40) NOT NULL,
+  PRIMARY KEY (IDList),
+  FOREIGN KEY (Author) REFERENCES Users(Username)
+);
+
+CREATE TABLE Contains
+(
+  Song INT NOT NULL,
+  List INT NOT NULL,
+  PRIMARY KEY (Song, List),
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong),
+  FOREIGN KEY (List) REFERENCES Playlists(IDList)
+);
+
+CREATE TABLE Genres
+(
+  Name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Name)
+);
+
+CREATE TABLE Relate
+(
+  Genre VARCHAR(20) NOT NULL,
+  Artist VARCHAR(40) NOT NULL,
+  PRIMARY KEY (Genre, Artist),
+  FOREIGN KEY (Genre) REFERENCES Genres(Name),
+  FOREIGN KEY (Artist) REFERENCES Artists(Username)
+);
+
+CREATE TABLE Belong
+(
+  Song INT NOT NULL,
+  Genre VARCHAR(20) NOT NULL,
+  PRIMARY KEY (Song, Genre),
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong),
+  FOREIGN KEY (Genre) REFERENCES Genres(Name)
+);
+
+CREATE TABLE Creates
+(
+  Song INT NOT NULL,
+  Username VARCHAR(40) NOT NULL,
+  PRIMARY KEY (Song, Username),
+  FOREIGN KEY (Song) REFERENCES Songs(IDSong),
+  FOREIGN KEY (Username) REFERENCES Artists(Username)
+);
